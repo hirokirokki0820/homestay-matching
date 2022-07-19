7fea980173783a0bdc2a6dd73b3076edeb1699c6
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-07-20 00:36:03 +0900

    Add: 新規登録フォームの作成, userプロフィール画面の作成, Gravatarの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :gender, :email, :password, :password_confirmation)
     end
 
 

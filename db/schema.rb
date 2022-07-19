8b14f3d34acb49113178ccdf5d79e91daf0d9444
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-07-19 15:51:25 +0900

    Add: 新規登録フォームの作成(viewのみ)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_17_050100) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_054553) do
   create_table "users", id: :string, force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "gender"
   end
 
 end

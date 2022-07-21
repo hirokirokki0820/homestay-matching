43173e9639758f621876925521d7be6fc535b5e8
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-07-21 18:07:39 +0900

    Update: ログイン失敗時にフォームに入力した値を保持するよう変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in(user)
-      flash[:notice] = "こんにちは、#{user.name}さん"
-      redirect_to user
+    @user = User.find_by(email: session_params[:email].downcase)
+    if @user && @user.authenticate(session_params[:password])
+      log_in(@user)
+      flash[:notice] = "こんにちは、#{@user.name}さん"
+      redirect_to @user
     else
+      @user = User.new(session_params)
       flash.now[:error] = "ログインに失敗しました"
       render "new", status: :unprocessable_entity
     end
@@ -18,4 +19,10 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_path, status: :see_other
   end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+
 end
